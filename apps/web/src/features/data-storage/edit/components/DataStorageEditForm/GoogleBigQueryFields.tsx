7473ac9d6368e653c4fb698e1e7047e673bbb989
Roman Dubovyi <r.dubovoi@owox.com>
2025-06-17 23:14:41 +0300

    Switch `react-hook-form` location input to `Controller` for improved integration

--- apps/web/src/features/data-storage/edit/components/DataStorageEditForm/GoogleBigQueryFields.tsx
+++ apps/web/src/features/data-storage/edit/components/DataStorageEditForm/GoogleBigQueryFields.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { Input } from '@owox/ui/components/input';
 import { Textarea } from '@owox/ui/components/textarea';
 import { DataStorageType } from '../../../shared';
@@ -60,65 +60,73 @@ export const GoogleBigQueryFields = ({ form }: GoogleBigQueryFieldsProps) => {
             <Label htmlFor='location' className='block text-sm font-medium text-gray-700'>
               Location
             </Label>
-            <Select {...register('config.location', { required: true })} defaultValue='US'>
-              <SelectTrigger id='location' className='flex w-full items-center'>
-                <SelectValue placeholder='Select a location' />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>North America</SelectLabel>
-                  <SelectItem value='US'>US (multiple regions)</SelectItem>
-                  <SelectItem value='northamerica-northeast1'>Montréal</SelectItem>
-                  <SelectItem value='northamerica-northeast2'>Toronto</SelectItem>
-                  <SelectItem value='us-central1'>Iowa</SelectItem>
-                  <SelectItem value='us-east1'>South Carolina</SelectItem>
-                  <SelectItem value='us-east4'>Northern Virginia</SelectItem>
-                  <SelectItem value='us-east5'>Columbus</SelectItem>
-                  <SelectItem value='us-west1'>Oregon</SelectItem>
-                  <SelectItem value='us-west2'>Los Angeles</SelectItem>
-                  <SelectItem value='us-west3'>Salt Lake City</SelectItem>
-                  <SelectItem value='us-west4'>Las Vegas</SelectItem>
-                </SelectGroup>
-                <SelectGroup>
-                  <SelectLabel>Europe</SelectLabel>
-                  <SelectItem value='EU'>EU (multiple regions)</SelectItem>
-                  <SelectItem value='europe-central2'>Warsaw</SelectItem>
-                  <SelectItem value='europe-north1'>Finland</SelectItem>
-                  <SelectItem value='europe-southwest1'>Madrid</SelectItem>
-                  <SelectItem value='europe-west1'>Belgium</SelectItem>
-                  <SelectItem value='europe-west2'>London</SelectItem>
-                  <SelectItem value='europe-west3'>Frankfurt</SelectItem>
-                  <SelectItem value='europe-west4'>Netherlands</SelectItem>
-                  <SelectItem value='europe-west6'>Zurich</SelectItem>
-                  <SelectItem value='europe-west8'>Milan</SelectItem>
-                  <SelectItem value='europe-west9'>Paris</SelectItem>
-                  <SelectItem value='europe-west12'>Turin</SelectItem>
-                </SelectGroup>
-                <SelectGroup>
-                  <SelectLabel>Asia Pacific</SelectLabel>
-                  <SelectItem value='asia-east1'>Taiwan</SelectItem>
-                  <SelectItem value='asia-east2'>Hong Kong</SelectItem>
-                  <SelectItem value='asia-northeast1'>Tokyo</SelectItem>
-                  <SelectItem value='asia-northeast2'>Osaka</SelectItem>
-                  <SelectItem value='asia-northeast3'>Seoul</SelectItem>
-                  <SelectItem value='asia-south1'>Mumbai</SelectItem>
-                  <SelectItem value='asia-south2'>Delhi</SelectItem>
-                  <SelectItem value='asia-southeast1'>Singapore</SelectItem>
-                  <SelectItem value='asia-southeast2'>Jakarta</SelectItem>
-                  <SelectItem value='australia-southeast1'>Sydney</SelectItem>
-                  <SelectItem value='australia-southeast2'>Melbourne</SelectItem>
-                </SelectGroup>
-                <SelectGroup>
-                  <SelectLabel>Other</SelectLabel>
-                  <SelectItem value='southamerica-east1'>São Paulo</SelectItem>
-                  <SelectItem value='southamerica-west1'>Santiago</SelectItem>
-                  <SelectItem value='me-central1'>Doha</SelectItem>
-                  <SelectItem value='me-central2'>Dammam</SelectItem>
-                  <SelectItem value='me-west1'>Tel Aviv</SelectItem>
-                  <SelectItem value='africa-south1'>Johannesburg</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+            <Controller
+              name='config.location'
+              control={form.control}
+              defaultValue='US'
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Select onValueChange={field.onChange} value={field.value}>
+                  <SelectTrigger id='location' className='flex w-full items-center'>
+                    <SelectValue placeholder='Select a location' />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>North America</SelectLabel>
+                      <SelectItem value='US'>US (multiple regions)</SelectItem>
+                      <SelectItem value='northamerica-northeast1'>Montréal</SelectItem>
+                      <SelectItem value='northamerica-northeast2'>Toronto</SelectItem>
+                      <SelectItem value='us-central1'>Iowa</SelectItem>
+                      <SelectItem value='us-east1'>South Carolina</SelectItem>
+                      <SelectItem value='us-east4'>Northern Virginia</SelectItem>
+                      <SelectItem value='us-east5'>Columbus</SelectItem>
+                      <SelectItem value='us-west1'>Oregon</SelectItem>
+                      <SelectItem value='us-west2'>Los Angeles</SelectItem>
+                      <SelectItem value='us-west3'>Salt Lake City</SelectItem>
+                      <SelectItem value='us-west4'>Las Vegas</SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Europe</SelectLabel>
+                      <SelectItem value='EU'>EU (multiple regions)</SelectItem>
+                      <SelectItem value='europe-central2'>Warsaw</SelectItem>
+                      <SelectItem value='europe-north1'>Finland</SelectItem>
+                      <SelectItem value='europe-southwest1'>Madrid</SelectItem>
+                      <SelectItem value='europe-west1'>Belgium</SelectItem>
+                      <SelectItem value='europe-west2'>London</SelectItem>
+                      <SelectItem value='europe-west3'>Frankfurt</SelectItem>
+                      <SelectItem value='europe-west4'>Netherlands</SelectItem>
+                      <SelectItem value='europe-west6'>Zurich</SelectItem>
+                      <SelectItem value='europe-west8'>Milan</SelectItem>
+                      <SelectItem value='europe-west9'>Paris</SelectItem>
+                      <SelectItem value='europe-west12'>Turin</SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Asia Pacific</SelectLabel>
+                      <SelectItem value='asia-east1'>Taiwan</SelectItem>
+                      <SelectItem value='asia-east2'>Hong Kong</SelectItem>
+                      <SelectItem value='asia-northeast1'>Tokyo</SelectItem>
+                      <SelectItem value='asia-northeast2'>Osaka</SelectItem>
+                      <SelectItem value='asia-northeast3'>Seoul</SelectItem>
+                      <SelectItem value='asia-south1'>Mumbai</SelectItem>
+                      <SelectItem value='asia-south2'>Delhi</SelectItem>
+                      <SelectItem value='asia-southeast1'>Singapore</SelectItem>
+                      <SelectItem value='asia-southeast2'>Jakarta</SelectItem>
+                      <SelectItem value='australia-southeast1'>Sydney</SelectItem>
+                      <SelectItem value='australia-southeast2'>Melbourne</SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Other</SelectLabel>
+                      <SelectItem value='southamerica-east1'>São Paulo</SelectItem>
+                      <SelectItem value='southamerica-west1'>Santiago</SelectItem>
+                      <SelectItem value='me-central1'>Doha</SelectItem>
+                      <SelectItem value='me-central2'>Dammam</SelectItem>
+                      <SelectItem value='me-west1'>Tel Aviv</SelectItem>
+                      <SelectItem value='africa-south1'>Johannesburg</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              )}
+            />
             {errors.config && 'location' in errors.config && (
               <p className='mt-1 text-sm text-red-600'>{errors.config.location?.message}</p>
             )}
