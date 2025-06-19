70c9da7f7a07a3d8f6ed2425648dff81ee31fd05
Roman Dubovyi <r.dubovoi@owox.com>
2025-06-19 20:58:27 +0300

    Add "New Data Mart" button to Data Marts page: - Introduced a secondary button with an icon for creating new Data Marts. - Updated header layout to accommodate the button.

--- apps/web/src/pages/data-marts/list/DataMartsPage.tsx
+++ apps/web/src/pages/data-marts/list/DataMartsPage.tsx
@@ -5,6 +5,9 @@ import {
 } from '../../../features/data-marts/list';
 import { useEffect } from 'react';
 import { getDataMartColumns } from '../../../features/data-marts/list/components/DataMartTable/columns.tsx';
+import { Button } from '@owox/ui/components/button';
+import { Plus } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const DataMartTableWithContext = () => {
   const { items, loadDataMarts } = useDataMartList();
@@ -19,8 +22,14 @@ const DataMartTableWithContext = () => {
 export default function DataMartsPage() {
   return (
     <div>
-      <header className='px-12 pt-6 pb-4'>
+      <header className='flex items-center justify-between px-12 pt-6 pb-4'>
         <h1 className='text-2xl font-medium'>Data Marts</h1>
+        <Link to={'/data-marts/create'}>
+          <Button variant='secondary' size='sm'>
+            <Plus className='mr-2 h-4 w-4' />
+            New Data Mart
+          </Button>
+        </Link>
       </header>
       <div className='px-4 sm:px-12'>
         <DataMartListProvider>
