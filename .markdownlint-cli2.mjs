41fc7eaff829ada44e1c17c1081c3d0cc98f5b74
Antony Ishchenko <antony.ishchenko@gmail.com>
2025-06-30 16:14:18 +0300

    (org): update markdownlint-cli config

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import markdownlintRules from './.markdownlint.json' with { type: 'json' };
 
+const args = process.argv.slice(2);
+const hasFileArguments = args.some(arg => !arg.startsWith('-'));
+
 const options = {
-  globs: [
-    '**/*.md', // Include all markdown files
-    '!**/node_modules/**', // Exclude all node_modules folders
-  ],
   config: markdownlintRules,
 };
+
+if (!hasFileArguments) {
+  options.globs = [
+    '**/*.md', // Include all markdown files
+    '!**/node_modules/**', // Exclude all node_modules folders
+  ];
+}
 
 export default options;
