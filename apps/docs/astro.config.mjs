991cc2c971af89212e2bec09332499b7ba999e9e
Andrew Laskevych <an.laskevych@gmail.com>
2025-07-15 19:41:34 +0300

    feat(docs): added gtm id from env

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,7 +5,7 @@ import rehypeExternalLinks from 'rehype-external-links';
 import starlightAutoSidebar from 'starlight-auto-sidebar';
 import { getConfig } from './scripts/env-config.js';
 
-const { site, base } = getConfig();
+const { site, base, gtmId } = getConfig();
 
 export default defineConfig({
   site,
@@ -31,7 +31,7 @@ export default defineConfig({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TVQPWGV6');`,
+})(window,document,'script','dataLayer','${gtmId}');`,
         },
       ],
       sidebar: [
