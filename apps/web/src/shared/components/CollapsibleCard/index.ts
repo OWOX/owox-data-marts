6e8fb41ca68c300afdfeabbfadc666f3f1bbb24e
Artem Fenoha <artfenoha@gmail.com>
2025-08-13 18:57:59 +0300

    refactor(web): streamline CollapsibleCard component structure and enhance header functionality

--- apps/web/src/shared/components/CollapsibleCard/index.ts
+++ apps/web/src/shared/components/CollapsibleCard/index.ts
@@ -1,2 +1,5 @@
 export * from './CollapsibleCard.tsx';
+export * from './CollapsibleCardHeader.tsx';
+export * from './CollapsibleCardHeaderTitle.tsx';
+export * from './CollapsibleCardHeaderActions.tsx';
 export * from './types.ts';
