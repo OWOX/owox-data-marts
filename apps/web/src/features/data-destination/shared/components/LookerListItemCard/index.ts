e4d71b710d59f182d3cb14ce9e3bb46f98e7da6f
Artem Fenoha <artfenoha@gmail.com>
2025-08-15 15:47:33 +0300

    feat(web): remove LookerListItemCard component and add LookerStudioReportCard component with sub-components

--- apps/web/src/features/data-destination/shared/components/LookerListItemCard/index.ts
+++
@@ -1,6 +0,0 @@
-export { LookerListItemCard } from './LookerListItemCard';
-export { LookerListItemCardActionLeft } from './LookerListItemCard';
-export { LookerListItemCardContent } from './LookerListItemCard';
-export { LookerListItemCardTitle } from './LookerListItemCard';
-export { LookerListItemCardDescription } from './LookerListItemCard';
-export { LookerListItemCardActionRight } from './LookerListItemCard';
---
+++ apps/web/src/features/data-marts/reports/list/components/LookerStudioReportCard/index.ts
@@ -0,0 +1,6 @@
+export { LookerStudioReportCard } from './LookerStudioReportCard';
+export { LookerStudioReportCardActionLeft } from './LookerStudioReportCard';
+export { LookerStudioReportCardContent } from './LookerStudioReportCard';
+export { LookerStudioReportCardTitle } from './LookerStudioReportCard';
+export { LookerStudioReportCardDescription } from './LookerStudioReportCard';
+export { LookerStudioReportCardActionRight } from './LookerStudioReportCard';
