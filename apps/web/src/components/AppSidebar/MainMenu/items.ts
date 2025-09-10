c167b877a386de8b23d2670486b45211bca4ae6c
Artem Fenoha <artfenoha@gmail.com>
2025-09-10 16:59:05 +0300

    refactor(web): restructure AppSidebar to use MainMenu component

---
+++ apps/web/src/components/AppSidebar/MainMenu/items.ts
@@ -0,0 +1,21 @@
+// sidebar-nav-items.ts
+import { Box, DatabaseIcon, ArchiveRestore } from 'lucide-react';
+import type { MainMenuItem } from './types';
+
+export const MainMenuItems: MainMenuItem[] = [
+  {
+    title: 'Data Marts',
+    url: '/data-marts',
+    icon: Box,
+  },
+  {
+    title: 'Storages',
+    url: '/data-storages',
+    icon: DatabaseIcon,
+  },
+  {
+    title: 'Destinations',
+    url: '/data-destinations',
+    icon: ArchiveRestore,
+  },
+];
