4d5242c42fa53c9615aa03fc277bb76847e369d5
Andrew Laskevych <an.laskevych@gmail.com>
2025-10-03 14:01:20 +0300

    fix(idp-better-auth): removed default value for `magicLinkTtl` in `createBetterAuthConfig`

--- packages/idp-better-auth/src/auth/auth-config.ts
+++ packages/idp-better-auth/src/auth/auth-config.ts
@@ -58,7 +58,7 @@ export async function createBetterAuthConfig(
           }
         ).lastToken = token;
       },
-      expiresIn: config.magicLinkTtl || 3600, // 1 hour
+      expiresIn: config.magicLinkTtl,
       disableSignUp: false,
     })
   );
