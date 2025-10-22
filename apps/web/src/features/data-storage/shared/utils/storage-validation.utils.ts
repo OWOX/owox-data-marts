71f3cec647871afeedb7ac22ea536445e02ad960
Roman Dubovyi <r.dubovoi@gmail.com>
2025-10-22 19:36:33 +0300

    feat(data-storage): add storage config validation util

---
+++ apps/web/src/features/data-storage/shared/utils/storage-validation.utils.ts
@@ -0,0 +1,19 @@
+import { DataStorageType } from '../model/types/data-storage-type.enum';
+import type { DataStorage } from '../model/types/data-storage';
+
+/**
+ * Validates the configuration of the provided data storage.
+ *
+ * @param {DataStorage} storage - The data storage configuration to validate. The storage object should contain a type and a corresponding config object.
+ * @return {boolean} Returns true if the storage configuration is valid based on its type, otherwise returns false.
+ */
+export function isDataStorageConfigValid(storage: DataStorage): boolean {
+  switch (storage.type) {
+    case DataStorageType.GOOGLE_BIGQUERY:
+      return Boolean(storage.config.projectId && storage.config.location);
+    case DataStorageType.AWS_ATHENA:
+      return Boolean(storage.config.region && storage.config.outputBucket);
+    default:
+      return false;
+  }
+}
