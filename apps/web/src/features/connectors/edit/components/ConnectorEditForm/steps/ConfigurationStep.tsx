12981d65bead95a29492c34021a4c93abb8e4c22
Roman Dubovyi <r.dubovoi@gmail.com>
2025-10-26 00:45:15 +0300

    fix(web): validation logic in ConfigurationStep

--- apps/web/src/features/connectors/edit/components/ConnectorEditForm/steps/ConfigurationStep.tsx
+++ apps/web/src/features/connectors/edit/components/ConnectorEditForm/steps/ConfigurationStep.tsx
@@ -100,11 +100,9 @@ export function ConfigurationStep({
     if (value === null || value === undefined) return false;
     if (typeof value === 'string' && value.trim() === '') return false;
     if (Array.isArray(value) && value.length === 0) return false;
-
-    return true;
   }, []);
 
-  const validateOneOfReqursive = useCallback(
+  const validateOneOfRecursive = useCallback(
     (value: unknown, spec: ConnectorSpecificationResponseApiDto): boolean => {
       if (typeof value !== 'object' || value === null) return validateValue(value);
 
@@ -115,7 +113,7 @@ export function ConfigurationStep({
         return oneOfs.every(oneOf =>
           Object.entries(oneOf.items).every(([, item]) => {
             if (oneOf.value in value) {
-              return validateOneOfReqursive((value as Record<string, unknown>)[oneOf.value], item);
+              return validateOneOfRecursive((value as Record<string, unknown>)[oneOf.value], item);
             }
             return false;
           })
@@ -129,17 +127,19 @@ export function ConfigurationStep({
 
   const validateConfiguration = useCallback(
     (config: Record<string, unknown>, specs: ConnectorSpecificationResponseApiDto[]) => {
-      const requeredOneOfSpecs = specs.filter(spec => spec.required && spec.oneOf);
-      const isValidOneOf = requeredOneOfSpecs.some(spec =>
-        validateOneOfReqursive(config[spec.name], spec)
-      );
+      const requiredOneOfSpecs = specs.filter(spec => spec.required && spec.oneOf);
+
+      const isValidOneOf =
+        requiredOneOfSpecs.length === 0
+          ? true
+          : requiredOneOfSpecs.some(spec => validateOneOfRecursive(config[spec.name], spec));
 
       const requiredSpecs = specs.filter(spec => spec.required && spec.name !== 'Fields');
       const isValidRequired = requiredSpecs.every(spec => validateValue(config[spec.name]));
 
       return isValidOneOf && isValidRequired;
     },
-    [validateValue, validateOneOfReqursive]
+    [validateValue, validateOneOfRecursive]
   );
 
   const handleSecretEditToggle = (name: string, enable: boolean) => {
