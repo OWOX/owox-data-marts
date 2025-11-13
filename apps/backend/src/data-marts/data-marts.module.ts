23ffe909dd604d3be618920342628642bcae0061
Oleksandr Kalnyk <aleksandr.kalnik@gmail.com>
2025-11-13 15:39:51 +0200

    feat(backend): utility controller for markdown preview

--- apps/backend/src/data-marts/data-marts.module.ts
+++ apps/backend/src/data-marts/data-marts.module.ts
@@ -4,6 +4,7 @@ import { DataMartController } from './controllers/data-mart.controller';
 import { DataStorageController } from './controllers/data-storage.controller';
 import { DataDestinationController } from './controllers/data-destination.controller';
 import { LookerStudioConnectorController } from './controllers/external/looker-studio-connector.controller';
+import { MarkdownParserController } from './controllers/markdown-parser.controller';
 import { ReportController } from './controllers/report.controller';
 import { InsightController } from './controllers/insight.controller';
 import { ScheduledTriggerController } from './controllers/scheduled-trigger.controller';
@@ -140,6 +141,7 @@ import { RetryInterruptedConnectorRunsProcessor } from './system-triggers/proces
     LookerStudioConnectorController,
     SqlDryRunTriggerController,
     SchemaActualizeTriggerController,
+    MarkdownParserController,
   ],
   providers: [
     ...dataStorageResolverProviders,
