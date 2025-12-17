6adfed9bcc1e5682ac9144d4c3b04edcc97e188e
Artem Fenoha <48394895+artemfenoga@users.noreply.github.com>
2025-12-17 14:32:59 +0200

    Docs: Added getting started video (#811)

---
+++ apps/web/src/components/ContentPopovers/popovers/Video3GettingStartedDataMarts.tsx
@@ -0,0 +1,17 @@
+export function Video3GettingStartedDataMarts() {
+  return (
+    <>
+      <video
+        src='https://github.com/user-attachments/assets/dcb60ce1-dd7b-4783-b19c-f2fd85079e1f'
+        controls
+        autoPlay
+        muted
+        loop
+        playsInline
+        className='w-full rounded-md'
+      >
+        Your browser does not support the video tag.
+      </video>
+    </>
+  );
+}
---
+++ apps/web/src/components/ContentPopovers/popovers/index.ts
@@ -0,0 +1,3 @@
+export * from './Video1GoogleSheetsReport';
+export * from './Video2LookerAsDestination';
+export * from './Video3GettingStartedDataMarts';
