81372c7b5aa728c137781af920ebc2b30cfeec41
Artem Fenoha <artfenoha@gmail.com>
2026-01-27 15:52:46 +0200

    refactor(web): Simplify InsightsTable component by integrating BaseTable and optimizing column definitions

---
+++ apps/web/src/features/data-marts/insights/components/InsightsTable/columns/columnLabels.ts
@@ -0,0 +1,6 @@
+import { InsightColumnKey } from './columnKeys';
+
+export const InsightColumnLabels: Record<InsightColumnKey, string> = {
+  [InsightColumnKey.TITLE]: 'Title',
+  [InsightColumnKey.LAST_RUN]: 'Last Run',
+};
