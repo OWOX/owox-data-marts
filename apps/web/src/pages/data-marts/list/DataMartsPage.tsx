32627eb7297c47496ee272d9b14aec47867e5d72
Artem Fenoha <artfenoha@gmail.com>
2026-02-09 19:41:42 +0200

    feat(web): add notification for legacy storage setup in Data Marts page

--- apps/web/src/pages/data-marts/list/DataMartsPage.tsx
+++ apps/web/src/pages/data-marts/list/DataMartsPage.tsx
@@ -7,10 +7,18 @@ import { useEffect } from 'react';
 import { getDataMartColumns } from '../../../features/data-marts/list/components/DataMartTable/columns/columns.tsx';
 import { ConnectorContextProvider } from '../../../features/connectors/shared/model/context';
 import { useConnector } from '../../../features/connectors/shared/model/hooks/useConnector.ts';
+import PageNotificationLegacyStorageSetup from './PageNotificationLegacyStorageSetup';
+import { DataMartStatus } from '../../../features/data-marts/shared/enums/data-mart-status.enum';
+import { DataStorageType } from '../../../features/data-storage/shared/model/types/data-storage-type.enum.ts';
 
 const DataMartTableWithContext = () => {
   const { items, loadDataMarts, deleteDataMart, refreshList, loading } = useDataMartList();
   const { connectors, fetchAvailableConnectors } = useConnector();
+  const hasDraftWithLegacyStorage = items.some(
+    item =>
+      item.status.code === DataMartStatus.DRAFT &&
+      item.storageType === DataStorageType.GOOGLE_BIGQUERY
+  );
 
   useEffect(() => {
     void fetchAvailableConnectors();
@@ -21,13 +29,16 @@ const DataMartTableWithContext = () => {
   }, [loadDataMarts]);
 
   return (
-    <DataMartTable
-      columns={getDataMartColumns({ connectors })}
-      data={items}
-      deleteDataMart={deleteDataMart}
-      refetchDataMarts={refreshList}
-      isLoading={loading}
-    />
+    <>
+      {hasDraftWithLegacyStorage && <PageNotificationLegacyStorageSetup />}
+      <DataMartTable
+        columns={getDataMartColumns({ connectors })}
+        data={items}
+        deleteDataMart={deleteDataMart}
+        refetchDataMarts={refreshList}
+        isLoading={loading}
+      />
+    </>
   );
 };
 
