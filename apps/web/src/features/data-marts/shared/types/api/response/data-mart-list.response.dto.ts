74400221a66b758114d2f0374f313152f71c7aa3
Oleksandr Kalnyk <aleksandr.kalnik@gmail.com>
2026-02-13 16:54:55 +0200

    feat(web): paginated data marts list requests

--- apps/web/src/features/data-marts/shared/types/api/response/data-mart-list.response.dto.ts
+++ apps/web/src/features/data-marts/shared/types/api/response/data-mart-list.response.dto.ts
@@ -1,6 +1,10 @@
 import type { DataMartResponseDto } from './data-mart.response.dto.ts';
 
 /**
- * Data transfer object for data mart list response
+ * Data transfer object for paginated data mart list response
  */
-export type DataMartListResponseDto = DataMartResponseDto[];
+export interface DataMartListResponseDto {
+  items: DataMartResponseDto[];
+  total: number;
+  nextOffset: number | null;
+}
