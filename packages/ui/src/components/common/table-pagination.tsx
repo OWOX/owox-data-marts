3207a7c00d48302cbc2d0568ed1c67647ce5597e
Artem Fenoha <artfenoha@gmail.com>
2026-02-25 14:17:57 +0200

    refactor(web): update table components for improved user feedback and layout adjustments

--- packages/ui/src/components/common/table-pagination.tsx
+++ packages/ui/src/components/common/table-pagination.tsx
@@ -53,73 +53,77 @@ export function TablePagination<TData>({
           </span>
         )}
       </div>
-      <div className='flex items-center space-x-6 lg:space-x-8'>
-        <div className='flex items-center space-x-2'>
-          <span>Rows per page</span>
-          <Select
-            value={`${table.getState().pagination.pageSize}`}
-            onValueChange={value => {
-              table.setPageSize(Number(value));
-            }}
-          >
-            <SelectTrigger className='bg-background h-8 w-[74px]'>
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
-            </SelectTrigger>
-            <SelectContent>
-              {pageSizeResult.map(pageSize => (
-                <SelectItem key={pageSize} value={`${pageSize}`}>
-                  {pageSize}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-        <div className='flex items-center justify-center'>
-          {from}–{to} of {total} {total === 1 ? 'row' : 'rows'}
-        </div>
-        <div className='flex items-center space-x-1'>
-          <Button
-            variant='outline'
-            size='icon'
-            className='hidden size-8 lg:flex'
-            onClick={() => table.setPageIndex(0)}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <span className='sr-only'>Go to first page</span>
-            <ChevronsLeft />
-          </Button>
-          <Button
-            variant='outline'
-            size='icon'
-            className='size-8'
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <span className='sr-only'>Go to previous page</span>
-            <ChevronLeft />
-          </Button>
-          <Button
-            variant='outline'
-            size='icon'
-            className='size-8'
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            <span className='sr-only'>Go to next page</span>
-            <ChevronRight />
-          </Button>
-          <Button
-            variant='outline'
-            size='icon'
-            className='hidden size-8 lg:flex'
-            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-            disabled={!table.getCanNextPage()}
-          >
-            <span className='sr-only'>Go to last page</span>
-            <ChevronsRight />
-          </Button>
-        </div>
-      </div>
+      {total > 0 && (
+        <>
+          <div className='flex items-center space-x-6 lg:space-x-8'>
+            <div className='flex items-center space-x-2'>
+              <span>Rows per page</span>
+              <Select
+                value={`${table.getState().pagination.pageSize}`}
+                onValueChange={value => {
+                  table.setPageSize(Number(value));
+                }}
+              >
+                <SelectTrigger className='bg-background h-8 w-[74px]'>
+                  <SelectValue placeholder={table.getState().pagination.pageSize} />
+                </SelectTrigger>
+                <SelectContent>
+                  {pageSizeResult.map(pageSize => (
+                    <SelectItem key={pageSize} value={`${pageSize}`}>
+                      {pageSize}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='flex items-center justify-center'>
+              {from}–{to} of {total} {total === 1 ? 'row' : 'rows'}
+            </div>
+            <div className='flex items-center space-x-1'>
+              <Button
+                variant='outline'
+                size='icon'
+                className='hidden size-8 lg:flex'
+                onClick={() => table.setPageIndex(0)}
+                disabled={!table.getCanPreviousPage()}
+              >
+                <span className='sr-only'>Go to first page</span>
+                <ChevronsLeft />
+              </Button>
+              <Button
+                variant='outline'
+                size='icon'
+                className='size-8'
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                <span className='sr-only'>Go to previous page</span>
+                <ChevronLeft />
+              </Button>
+              <Button
+                variant='outline'
+                size='icon'
+                className='size-8'
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                <span className='sr-only'>Go to next page</span>
+                <ChevronRight />
+              </Button>
+              <Button
+                variant='outline'
+                size='icon'
+                className='hidden size-8 lg:flex'
+                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                disabled={!table.getCanNextPage()}
+              >
+                <span className='sr-only'>Go to last page</span>
+                <ChevronsRight />
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
