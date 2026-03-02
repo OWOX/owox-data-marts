50c241bab863958f8addc15e3740ddd3f8aac428
Artem Fenoha <artfenoha@gmail.com>
2026-03-02 18:50:09 +0200

    refactor(web): update legacy storage setup notification text and instructions for Data Marts

--- apps/web/src/pages/data-marts/list/PageNotificationLegacyStorageSetup.tsx
+++ apps/web/src/pages/data-marts/list/PageNotificationLegacyStorageSetup.tsx
@@ -18,7 +18,7 @@ export default function PageNotificationLegacyStorageSetup() {
       <CollapsibleCard collapsible name='notification-legacy-storage-setup'>
         <CollapsibleCardHeader>
           <CollapsibleCardHeaderTitle icon={Info}>
-            Finish setting up Data Marts from the Google Sheets extension
+            Complete BigQuery storage setup to publish your Data Marts
           </CollapsibleCardHeaderTitle>
           <CollapsibleCardHeaderActions>
             <p className='text-muted-foreground/75 text-sm'>Action required</p>
@@ -27,7 +27,7 @@ export default function PageNotificationLegacyStorageSetup() {
         <CollapsibleCardContent>
           <div className='rounded-md border-b border-gray-200 bg-white dark:border-white/4 dark:bg-white/1'>
             <div className='flex flex-col gap-4 p-4 text-sm xl:p-6'>
-              <div className='flex flex-col gap-2 xl:text-center'>
+              <div className='flex flex-col gap-2'>
                 <p>
                   Your Data Marts created with{' '}
                   <a
@@ -38,16 +38,27 @@ export default function PageNotificationLegacyStorageSetup() {
                   >
                     OWOX Reports
                   </a>{' '}
-                  (Google Sheets extension) are listed below and currently in the{' '}
+                  (Google Sheets extension) are listed below in{' '}
                   <span className='font-semibold'>Draft</span> status.
                 </p>
                 <p>
-                  To activate and publish them, complete a one-time BigQuery storage setup.
-                  You&apos;ll select a storage, grant access, and then publish your Data Marts in
-                  just a few steps.
+                  To manage Data Marts in one place, get Insights and sync data to multiple
+                  destinations, complete the following steps:
                 </p>
+                <ol className='ml-4 flex list-inside list-decimal flex-col gap-1 text-left'>
+                  <li>
+                    <span className='font-semibold'>Select</span> a storage
+                  </li>
+                  <li>
+                    <span className='font-semibold'>Grant access</span> to Google BigQuery (used in
+                    OWOX extension)
+                  </li>
+                  <li>
+                    <span className='font-semibold'>Publish</span> your Data Marts
+                  </li>
+                </ol>
               </div>
-              <div className='flex items-center gap-2 xl:justify-center'>
+              <div className='flex items-center gap-2'>
                 <Button asChild>
                   <Link
                     to={scope(
@@ -55,7 +66,7 @@ export default function PageNotificationLegacyStorageSetup() {
                     )}
                   >
                     <Database className='size-4' />
-                    Choose a BigQuery storage
+                    Select a storage
                   </Link>
                 </Button>
                 <Button variant='outline' asChild>
