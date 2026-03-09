4baa9c05613dea9714c971078b67eaace2af11ca
Roman Dubovyi <r.dubovoi@gmail.com>
2026-03-10 01:33:27 +0200

    fix(web): update icon components to use camelCase for SVG attributes

--- apps/web/src/shared/icons/email-icon.tsx
+++ apps/web/src/shared/icons/email-icon.tsx
@@ -21,9 +21,9 @@ export const EmailIcon = ({
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
       stroke={strokeColor}
-      stroke-width={strokeWidth}
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth={strokeWidth}
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
       <path d='m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7' />
       <rect x='2' y='4' width='20' height='16' rx='2' />
