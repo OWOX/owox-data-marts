55ff80008980f24f665321f55886366f675ca786
prokopetsv <victoriasvvt@gmail.com>
2026-03-27 20:57:34 +0200

    revert previous changes and add needed one

--- packages/connectors/src/Core/AbstractConnector.js
+++ packages/connectors/src/Core/AbstractConnector.js
@@ -290,12 +290,7 @@ var AbstractConnector = class AbstractConnector {
         return lookbackDate;
       }
 
-      // First run: no state exists
-      // Use StartDate if configured, otherwise start from the 1st of last month
-      if (this.config.StartDate && this.config.StartDate.value) {
-        return this.config.StartDate.value;
-      }
-
+      // First run: no state exists, start from the 1st of last month
       const today = new Date();
       return new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth() - 1, 1));
     }
