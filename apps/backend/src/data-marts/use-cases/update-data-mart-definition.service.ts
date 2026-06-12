3e584c1deb9727ca7a5625d24d764caa503db1df
Roman Dubovyi <r.dubovoi@gmail.com>
2026-06-12 20:06:11 +0300

    refactor(backend): allow saving data mart definition without storage validation

--- apps/backend/src/data-marts/use-cases/update-data-mart-definition.service.ts
+++ apps/backend/src/data-marts/use-cases/update-data-mart-definition.service.ts
@@ -2,7 +2,6 @@ import { Injectable, ForbiddenException } from '@nestjs/common';
 import { BusinessViolationException } from '../../common/exceptions/business-violation.exception';
 import { OwoxEventDispatcher } from '../../common/event-dispatcher/owox-event-dispatcher';
 import { DataStorageType } from '../data-storage-types/enums/data-storage-type.enum';
-import { DataMartDefinitionValidatorFacade } from '../data-storage-types/facades/data-mart-definition-validator-facade.service';
 import { DataMartDto } from '../dto/domain/data-mart.dto';
 import { UpdateDataMartDefinitionCommand } from '../dto/domain/update-data-mart-definition.command';
 import { ConnectorDefinition } from '../dto/schemas/data-mart-table-definitions/connector-definition.schema';
@@ -20,7 +19,6 @@ import { AccessDecisionService, EntityType, Action } from '../services/access-de
 export class UpdateDataMartDefinitionService {
   constructor(
     private readonly dataMartService: DataMartService,
-    private readonly definitionValidatorFacade: DataMartDefinitionValidatorFacade,
     private readonly mapper: DataMartMapper,
     private readonly connectorSecretService: ConnectorSecretService,
     private readonly legacyDataMartsService: LegacyDataMartsService,
@@ -126,13 +124,6 @@ export class UpdateDataMartDefinitionService {
       );
     } else {
       dataMart.definition = command.definition;
-    }
-
-    if (
-      dataMart.definitionType !== DataMartDefinitionType.SQL &&
-      dataMart.definitionType !== DataMartDefinitionType.CONNECTOR
-    ) {
-      await this.definitionValidatorFacade.checkIsValid(dataMart);
     }
 
     await this.dataMartService.save(dataMart);
